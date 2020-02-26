9f75c342b0564b873834937cd543c7a2af1acee7
Technochips <thetechnochips@protonmail.com>
2020-02-26 11:37:43 +0100

    yikes forgot the actual png

---
+++ png/Chunk.cs
@@ -0,0 +1,64 @@
+﻿#pragma warning disable 0649
+using Force.Crc32;
+using System;
+using System.Text;
+
+namespace wadder.png
+{
+	class Chunk
+	{
+		public string type;
+		public byte[] data;
+
+		public Chunk(byte[] data)
+		{
+			byte[] r = new byte[4];
+			Array.Copy(data, 0, r, 0, 4);
+			if (BitConverter.IsLittleEndian) Array.Reverse(r);
+			int length = BitConverter.ToInt32(r, 0);
+
+			type = Encoding.ASCII.GetString(data, 4, 4);
+			this.data = new byte[length];
+			for(int i = 0; i < length; i++)
+			{
+				this.data[i] = data[i+8];
+			}
+		}
+		public Chunk(string type)
+		{
+			this.type = type;
+		}
+
+		public byte[] CalculateCRC()
+		{
+			byte[] input = new byte[data.Length + 4];
+
+			byte[] type = Encoding.ASCII.GetBytes(this.type);
+			for (int i = 0; i < 4; i++) input[i] = type[i];
+
+			for(int i = 0; i < data.Length; i++) input[i + 4] = data[i];
+
+			byte[] r = BitConverter.GetBytes(Crc32Algorithm.Compute(input));
+			Array.Reverse(r);
+			return r;
+		}
+		public byte[] GetData()
+		{
+			byte[] input = new byte[data.Length + 12];
+			//length
+			byte[] a = BitConverter.GetBytes(data.Length);
+			if (BitConverter.IsLittleEndian) Array.Reverse(a);
+			for (int i = 0; i < 4; i++) input[i] = a[i];
+			//type
+			a = Encoding.ASCII.GetBytes(type);
+			for (int i = 0; i < 4; i++) input[i+4] = a[i];
+			//data
+			for (int i = 0; i < data.Length; i++) input[i+8] = data[i];
+			//crc
+			a = CalculateCRC();
+			for(int i = 0; i < 4; i++) input[i+8+data.Length] = a[i];
+
+			return input;
+		}
+	}
+}
---
+++ png/PNG.cs
@@ -0,0 +1,89 @@
+﻿#pragma warning disable 0649
+using System;
+using System.Collections.Generic;
+
+namespace wadder.png
+{
+	class PNG
+	{
+		public byte[] header;
+		public List<Chunk> chunks;
+		public byte[] data;
+
+		public PNG(byte[] data)
+		{
+			this.data = data;
+			this.chunks = new List<Chunk>();
+			header = new byte[8];
+			for(int i = 0; i < 8; i++)
+			{
+				header[i] = data[i];
+			}
+			int p = 8;
+			while(p < data.Length)
+			{
+				byte[] l = new byte[4];
+				Array.Copy(data, p, l, 0, 4);
+				if (BitConverter.IsLittleEndian) Array.Reverse(l);
+				int length = BitConverter.ToInt32(l, 0);
+
+				byte[] r = new byte[length+12];
+				Array.Copy(data, p, r, 0, length + 12);
+				if(r.Length > 0) chunks.Add(new Chunk(r));
+				p += length + 12;
+			}
+		}
+		public byte[] RewriteData()
+		{
+			List<byte> data = new List<byte>();
+			foreach(byte b in header) data.Add(b);
+
+			foreach (Chunk chunk in chunks)
+			{
+				byte[] d = chunk.GetData();
+				foreach (byte b in d) data.Add(b);
+			}
+
+			this.data = data.ToArray();
+			return this.data;
+		}
+		public void AddOffset(int x, int y)
+		{
+			int p = -1;
+			bool exists = false;
+			for(int i = 0; i < chunks.Count; i++)
+			{
+				if(chunks[i].type == "IHDR")
+				{
+					p = i;
+				}
+				else if(chunks[i].type == "grAb")
+				{
+					p = i;
+					exists = true;
+					break;
+				}
+			}
+			if (p == -1) return;
+
+			Chunk c = new Chunk("grAb");
+			byte[] bx = BitConverter.GetBytes(x);
+			byte[] by = BitConverter.GetBytes(y);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(bx);
+				Array.Reverse(by);
+			}
+			c.data = new byte[8];
+			for (int j = 0; j < 4; j++)
+			{
+				c.data[j] = bx[j];
+				c.data[j + 4] = by[j];
+			}
+			if (exists)
+				chunks[p] = c;
+			else
+				chunks.Insert(p + 1, c);
+		}
+	}
+}
